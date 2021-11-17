ce4b772daffaf96e1be4ce2d4376f04d5e233d5d
bsamu <bsamuel0220@gmail.com>
2021-11-17 22:57:22 +0100

    script works for any length

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const loadEvent = function() {
     console.log("Az oldal betöltődött.");
 
+    // console.log(generateArray(100));
+    // console.log(generateArray(15));
+    // console.log(generateArray("stringelt kiskutya"));
+    // console.log(generateArray(true));
+    // console.log(generateArray([2, 5]));
+    // console.log(generateArray(15e3)); // hehe
+
     const root = document.getElementById("root");
     const list = generateArray(input);
 
@@ -15,9 +22,16 @@ const loadEvent = function() {
 const generateArray = function(amount) {
     let returnArray = [];
 
-    amount = parseInt(amount);
+    
+    // let amountConverted = parseInt(amount)
+    // amount = amountConverted; // ua, mint lenti
+    
+
+    amount = parseInt(amount); // ha nem szám = NaN, de a NaN (Not a Number) is egy szám
+    //console.log(amount);
 
     if (!Number.isNaN(amount)) {
+        //console.log("Az amount egy szám.");
         for (let i = 0; i < amount; i++) {
             if (i > 1000) {
                 returnArray.push(`${i}`);
@@ -30,6 +44,7 @@ const generateArray = function(amount) {
             }
         }
     } else {
+        // console.log("Az amount NEM egy szám.");
         returnArray.push("error");
     }
 
@@ -67,3 +82,15 @@ const generateArray = function(amount) {
 }
 
 window.addEventListener("load", loadEvent);
+
+/* 
+window.addEventListener("load", function() {
+    ez is ugyanaz kb, mint a fenti, ált a loadEventet nem akarjuk többször meghívni
+});
+*/
+
+/* 
+window.addEventListener("load", () => {
+    ez is ugyanaz kb, mint a fenti, ált a loadEventet nem akarjuk többször meghívni
+});
+*/
